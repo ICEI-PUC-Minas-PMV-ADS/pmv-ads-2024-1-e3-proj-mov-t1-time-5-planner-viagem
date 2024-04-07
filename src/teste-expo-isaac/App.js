346974341937a8048df42899cf6519bdbdc7f67f
Isaac Heder <112509611+isaackaiban@users.noreply.github.com>
2024-04-07 01:33:09 -0300

    create project isaac

---
+++ src/teste-expo-isaac/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+import HomePage from './src/components/pages/HomePage'
+
+const App = () => {
+
+  return(
+    <HomePage />
+  )
+
+}
+
+export default App;
---
+++ src/teste-expo-isaac/src/components/pages/HomePage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const HomePage = () => {
+
+  return (
+    <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+      <Text>teste by isaac</Text>
+    </View>
+  )
+}
+
+export default HomePage;
