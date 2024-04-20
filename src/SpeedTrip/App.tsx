15543612daf7206c97fd06cc2a71ddc3cdd322b5
mfernandasilva <mariasalomao2002@gmail.com>
2024-04-19 22:21:48 -0300

    Telas Despesas

--- src/SpeedTrip/App.tsx
+++ src/SpeedTrip/App.tsx
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import NotesListScreen from './screens/NotasListasScreen';
+import DespesasScreen from './screens/DespesasScreen';
+import CriarDespesasScreen from './screens/CriarDespesasScreen';
 
 const {Navigator, Screen} = createNativeStackNavigator();
 
@@ -12,7 +14,10 @@ export default function App() {
       <Navigator initialRouteName="Home">
         <Screen name="Home" component={HomeScreen} />
         <Screen name="Notas/Listas" component={NotesListScreen} />
+        <Screen name="Despesas" component={DespesasScreen} />
+        <Screen name="CriarDespesas" component={CriarDespesasScreen} />
       </Navigator>
     </NavigationContainer>
   );
 }
+
